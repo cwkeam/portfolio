cf082de1d693706e0f8e7b77a269b422c20055af
Chan Woo Kim <kim.chanwoo@yahoo.com>
2018-04-29 12:19:28 +0800

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '/');
+
+var app = express();
+var port = process.env.PORT || 8000;
+
+app.use('/', express.static(publicPath));
+
+
+app.get('/', (req, res)=>{
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/projects', (req, res)=>{
+	res.sendFile(path.join(__dirname + '/projects.html'));
+})
+
+
+app.listen(port, () => {
+  console.log('port is up and running at 8000');
+});
