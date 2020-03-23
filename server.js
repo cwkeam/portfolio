9eed31f69484c8568ec6a37d83c6c16816fed090
Chan Woo Kim <chanwookim@Chans-MacBook-Pro.local>
2020-03-23 19:06:33 +0900

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const publicPath = path.join(__dirname, '/');
+const publicPath = path.join(__dirname, '/public');
 
 var app = express();
 var port = process.env.PORT || 3000;
